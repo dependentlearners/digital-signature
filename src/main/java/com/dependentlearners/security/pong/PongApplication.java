92bd919dd8561cc449d081e72a73a2e4babd6b5f
Naresh Kumar Reddy <gaddam.1987@gmail.com>
2017-01-31 23:34:50 +0100

    Dummy class to push some thing
    Signed-off-by: Naresh Kumar Reddy <gaddam.1987@gmail.com>

---
+++ src/main/java/com/dependentlearners/security/pong/PongApplication.java
@@ -0,0 +1,23 @@
+package com.dependentlearners.security.pong;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@SpringBootApplication
+@Controller
+public class PongApplication {
+
+
+    @RequestMapping("/*")
+    @ResponseBody
+    public String pong() {
+        return "pong";
+    }
+
+    public static void main(String[] args) {
+        SpringApplication.run(PongApplication.class);
+    }
+}
